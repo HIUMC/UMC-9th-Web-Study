2950a747bf7414b34378aaddb9c0fd263a5b800a
박상현 <noeyhgnas@bagsanghyeon-ui-MacBookPro.local>
2025-09-18 18:01:42 +0900

    feat: TodoSection 컴포넌트 추가 (할 일/완료 리스트 섹션)

---
+++ shawn/week2/src/components/TodoSection.tsx
@@ -0,0 +1,43 @@
+import type { Todo } from "../../../week1/chapter-2/src/App";
+
+type TodoSectionProps = {
+  title: string;
+  todos: Todo[];
+  onComplete: (id: number) => void;
+  onDelete: (id: number) => void;
+};
+
+export default function TodoSection({
+  title,
+  todos,
+  onComplete,
+  onDelete,
+}: TodoSectionProps) {
+  return (
+    <section className="todo-section">
+      <h2 className="todo-section__title">{title}</h2>
+      <ul className="todo-section__list">
+        {todos.map((todo) => (
+          <li className="todo-item" key={todo.id}>
+            <span className="todo-item__text">{todo.text}</span>
+            {todo.isDone ? (
+              <button
+                className="todo-item__button todo-item__button--delete"
+                onClick={() => onDelete(todo.id)}
+              >
+                삭제
+              </button>
+            ) : (
+              <button
+                className="todo-item__button todo-item__button--complete"
+                onClick={() => onComplete(todo.id)}
+              >
+                완료
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
