49dd0821520eb3019c0c2e2857d5677a8a05ee32
j2nooh <wisestorm01@gmail.com>
2025-09-21 10:50:07 +0900

    W1 todo list JS 연동 오류 해결

--- tellie/week1/dist/script.js
+++ tellie/week1/dist/script.js
@@ -1,13 +1,10 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// 1. HTML 요소 선택 (핸드북 자바스크립트 편 참고)
 const todoInput = document.getElementById('todo-input');
 const todoForm = document.getElementById('todo-form');
 const todoList = document.getElementById('todo-list');
 const doneList = document.getElementById('done-list');
 let todos = [];
 let doneTasks = [];
-// - 할 일 목록 렌더링 하는 함수 정의
 const renderTasks = () => {
     todoList.innerHTML = "";
     doneList.innerHTML = "";
@@ -20,28 +17,23 @@ const renderTasks = () => {
         doneList.appendChild(li);
     });
 };
-// 3. 할 일 텍스트 입력 처리 함수. (공백 잘라줌)
 const getTodoText = () => {
     return todoInput.value.trim();
 };
-// 4. 할 일 추가 처리 함수
 const addTodo = (text) => {
     todos.push({ id: Date.now(), text });
     todoInput.value = '';
     renderTasks();
 };
-// 5. 할 일 상태 변경 (완료로 이동)
 const completeTodo = (todo) => {
     todos = todos.filter((t) => t.id !== todo.id);
     doneTasks.push(todo);
     renderTasks();
 };
-// 6. 완료된 할 일 삭제 함수
 const deleteTodo = (todo) => {
     doneTasks = doneTasks.filter((t) => t.id !== todo.id);
     renderTasks();
 };
-// 7. 할 일 아이템 생성 함수 (완료 여부에 따라 버튼 텍스트나 색상 설정)
 const createTodoElement = (todo, isDone) => {
     const li = document.createElement('li');
     li.classList.add('render-container__item');
@@ -67,7 +59,6 @@ const createTodoElement = (todo, isDone) => {
     li.appendChild(button);
     return li;
 };
-// 8. 폼 제출 이벤트 리스너
 todoForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const text = getTodoText();
@@ -75,4 +66,3 @@ todoForm.addEventListener('submit', (event) => {
         addTodo(text);
     }
 });
-//# sourceMappingURL=script.js.map
