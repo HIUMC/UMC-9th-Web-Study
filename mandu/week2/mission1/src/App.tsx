50af3091ac673240b11d815e66df28bdb031e244
tablemin03 <vs201466@naver.com>
2025-09-23 16:24:34 +0900

    refactor: Form.tsx를 TodoForm.tsx로 파일명 변경

--- mandu/week2/mission1/src/App.tsx
+++ mandu/week2/mission1/src/App.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import Form from './components/Form';
+import Form from './components/TodoForm';
 import TodoList from './components/TodoList';
 import './App.css'
 
