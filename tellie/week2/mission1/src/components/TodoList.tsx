e4cdbdfde4530fec3d120d21f9b95c9489e349bf
j2nooh <wisestorm01@gmail.com>
2025-09-26 23:12:23 +0900

    fix: week2 mission1 error

--- tellie/week2/mission1/src/components/TodoList.tsx
+++ tellie/week2/mission1/src/components/TodoList.tsx
@@ -11,7 +11,7 @@ interface TodoListProps {
 const TodoList = ({title, todos, buttonLabel, buttonColor, onClick,}: TodoListProps) => {
   return (
     <div className='render-container__section'>
-      <h2 className='render-container__title'>할 일</h2>
+      <h2 className='render-container__title'>{title}</h2>
       <ul id="todo-list" className="render-container__list">
         {todos?.map((todo) => (
           <li key={todo.id} className="render-container__item">
@@ -20,7 +20,6 @@ const TodoList = ({title, todos, buttonLabel, buttonColor, onClick,}: TodoListPr
           </li>
         ))}
       </ul>
-
     </div>
   );
 };
