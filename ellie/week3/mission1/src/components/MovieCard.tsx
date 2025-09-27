c2f932ee051dd5a7b2c94e4badb917be6bb14c3d
jangmk05 <jangmk2005@naver.com>
2025-09-28 01:11:45 +0900

    주석 추가

--- ellie/week3/mission1/src/components/MovieCard.tsx
+++ ellie/week3/mission1/src/components/MovieCard.tsx
@@ -1,13 +1,29 @@
 import { useState } from "react";
 import type { Movie } from "../types/movie"
 
+// 이 컴포넌트가 받을 props 형태 지정
 interface MovieCardProps {
   movie: Movie;
 }
 
 export default function MovieCard({movie}:MovieCardProps) {
+
+  // isHovered : 마우스를 카드위에 올려놨는가? -> 초기값 : false
   const [isHovered, setIsHovered] = useState(false);
 
+  // 1) 카드 컨테이너 div
+  //    - 영화 설명 부분(absolute)이 div를 기준으로 배치되도록
+  //    - onMouseEnter(마우스를 올리면) : isHovered=true
+  //    - onMouseLeate(마우스가 떠나면) : isHovered=false
+  // 2) 영화 포스터 이미지
+  //    - 이미지 링크는 poster_path
+  //    +) 상세페이지 만들 땐 backdrop_path 가져오면 될까..
+  // 3) hover시 바뀌는
+  //    - {isHovered && (...)} : 조건부 렌더링
+  //     -> 마우스를 올렸을 떄만 보이도록
+  //    - absolute : div 컨테이너를 벗어나지 않도록
+  //    - <h2> : 영화 제목
+  //    - <p> : 영화 줄거리
   return( 
   <div 
     className="relative rounded-xl shadow-lg overflow-hidden cursor-pointer w-44 transition-transform duration-500 hover:scale-105" 
@@ -22,7 +38,7 @@ export default function MovieCard({movie}:MovieCardProps) {
 
     {isHovered && (
       <div className="absolute inset-0 bg-gradient-to-t from-black/50
-       to-transparent backdrop-blur-md flex flex-col justify-center item-center p-4 text-white">
+       to-transparent backdrop-blur-md flex flex-col justify-center items-center p-4 text-white">
         <h2 className="text-lg font-bold leading-snug">{movie.title}</h2>
         <p className="text-sm text-gray-300 leading-relaxed mt-2 line-clamp-5">{movie.overview}</p>
       </div>
@@ -30,3 +46,35 @@ export default function MovieCard({movie}:MovieCardProps) {
   </div>
   );
 }
+
+// 🌸🌸tailwind css🌸🌸
+
+// 1) div 컨테이너
+//"relative : 이 요소를 상대적 위치 기준으로 설정
+// rounded-xl : 모서리 둥글게 대왕 동글
+// shadow-lg : 요소에 큰 그림자 추가
+// overflow-hidden : 자식 요소가 부모영역 벗어나면 잘라냄
+// cursor-pointer : 커서모양 포인터로
+// w-44 : 요소 너비 고정
+// transition-transform : 변화가 부드럽게 바뀌도록 
+// duration-500 : transition 시간을 0.5초(500ms)로 설정
+// hover:scale-105" ; 마우스 올리면 살짝(105%) 확대되게
+
+// 2) isHovered <div> - 컨테이너
+// "absolute : 부모박스(relative) 기준으로 위치가 잡힘
+// inset-0 : 부모의 전체 영역 꽉 채움
+// bg-gradient-to-t from-black/50 to-transparent : 배경을 아래에서 위로 향하는 그라데이션/ 시작색/ 끝색
+// backdrop-blur-md : 중간정도 블러처리
+// flex flex-col justify-center item-center : flex 중앙 정렬 
+// p-4 : 패딩 4
+// text-white" : 글씨 흰색
+
+// 3) isHovered <h2> - 영화 제목
+// "text-lg : 큰 글씨 
+// font-bold : 두꺼운 글씨
+// leading-snug" : 줄간격 살짝 좁게
+
+// 4) isHovered <p> - 줄거리 요약
+// leading-relaxed : 줄간격 여유있게
+// mt-2 : margin-top
+// line-clamp-5" : 텍스트를 최대 5줄까지만 표시
--- ellie/week3/mission1/src/pages/MoviePage.tsx
+++ ellie/week3/mission1/src/pages/MoviePage.tsx
@@ -4,11 +4,24 @@ import type { Movie } from "../types/movie";
 import MovieCard from "../components/MovieCard";
 
 export default function MoviePage() {
+  // 영화 목록 상태 변수
+  // moive[] 타입 지정으로 TMDB API에서 오는 데이터 구조 보장
+  // 초기값 빈배열
   const [movies,setMovies] = useState<Movie[]>([]);
 
+  // useEffect(...,[]) : 두번째 인자가 빈 배열?
+  //  -> 컴포넌트가 처음 렌더링될 때 한 번만 실행하고 싶을 때
+  // useEffect함수에서 api 호출
   useEffect(() => {
-    const fetchMovies = async () => {
+
+    // 1) API 호출
+    // 2) 응답 데이터 구조 분해
+    // 3) 상태 업데이트 과정
+    const fetchMovies = async () => { // async 키워드 : 함수 안에서 await 사용 가능
+      // axios : HTTP GET 요청을 보냄
+      // awat : 요청 완료까지 기다리고 결과를 data에 구조분해 할당
       const {data} = await axios('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+        // 요청 설정 (헤더에 인증토큰 추가해야함)
         {
           headers: {
             Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
@@ -16,12 +29,18 @@ export default function MoviePage() {
           },
         }
       );
-      
+    // 받아온 영화 배열을 movies상태에 저장
+    // 상태가 바뀌면 컴포넌트도 리렌더링
       setMovies(data.results);
     };
+    // 함수 호출
     fetchMovies();
   },[]);
 
+  // 렌더링!!
+  // tailwind 사용 : 패딩 10 display:grid sm일 때 한줄에 3개 오게 ... 반응형
+  // movies?.map(...) : movies 배열을 순회하면서 MovieCard 컴포넌트 생성
+  //  -> 옵셔널체이닝 : movies값이 null이나 undefined일 수도 있는 상황 대비
   return <div className="p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
     {movies?.map((movie) => 
       <MovieCard key={movie.id} movie={movie}/>
