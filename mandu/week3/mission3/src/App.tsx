6e449a0499fba0f1c46142874e8a3652210d2189
tablemin03 <vs201466@naver.com>
2025-09-28 18:18:33 +0900

    add: mission3 추가

---
+++ mandu/week3/mission3/src/App.tsx
@@ -0,0 +1,51 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from './pages/home';
+import NotFound from './pages/not-found';
+import Movies from './pages/movies';
+import RootLayout from './layout/root-layout';
+import NowPlaying from './pages/nowPlaying';
+import TopRated from './pages/topRated';
+import UpComing from './pages/upComing';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+        errorElement: <NotFound />,
+      },
+      {
+        path: 'movies',
+        element: <Movies />,
+        errorElement: <NotFound />,
+      },
+      {
+        path: 'now_playing',
+        element: <NowPlaying />,
+        errorElement: <NotFound />,
+      },
+      {
+        path: 'top_rated',
+        element: <TopRated />,
+        errorElement: <NotFound />,
+      },
+      {
+        path: 'up_coming',
+        element: <UpComing />,
+        errorElement: <NotFound />,
+      },
+    ]
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
