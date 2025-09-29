52f9de7d3aa48c7f74b7c74c21e44cbe52c09bb0
jangmk05 <jangmk2005@naver.com>
2025-09-29 10:07:49 +0900

    로딩스피너 라이브러리 이용

--- ellie/week3/mission3/src/components/LoadingSpinner.tsx
+++ ellie/week3/mission3/src/components/LoadingSpinner.tsx
@@ -1,5 +1,11 @@
-export const LoadingSpinner = () => {
-  return <div className="size-12 animate-spin rounded-full border-6 border-t-transparent" role="status">
+import { ClipLoader } from "react-spinners";
+
+export function LoadingSpinner() {
+  return <ClipLoader  className="size-12 animate-spin rounded-full border-6 border-t-transparent" >
     <span className="sr-only">로딩 중 ...</span>
-  </div>
-};
+  </ClipLoader>;
+}
+
+// return <div className="size-12 animate-spin rounded-full border-6 border-t-transparent" role="status">
+//     <span className="sr-only">로딩 중 ...</span>
+//   </div>
