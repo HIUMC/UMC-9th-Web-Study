a989ca2f23f1dffe10b5f900a182118222929cf0
jangmk05 <jangmk2005@naver.com>
2025-09-29 10:08:53 +0900

    LINKS 상수 분리

--- ellie/week3/mission3/src/components/Navbar.tsx
+++ ellie/week3/mission3/src/components/Navbar.tsx
@@ -1,15 +1,16 @@
 import {NavLink } from "react-router-dom"
+import { LINKS } from "../constants/LINKS"
 
 // 메뉴 데이터 배열 : 네비게이션 항목을 객체 배열로
 //  - to : 이동할 경로(URL)
 //  - label : 화면에 표시될 텍스트
-const LINKS = [
-  { to: '/', label: '홈'},
-  { to: '/movies/popular', label: '인기 영화'},
-  { to: '/movies/now_playing', label: '상영중'},
-  { to: '/movies/top_rated', label: '평점 높은'},
-  { to: '/movies/upcoming', label: '개봉 예정'},
-]
+// const LINKS = [
+//   { to: '/', label: '홈'},
+//   { to: '/movies/popular', label: '인기 영화'},
+//   { to: '/movies/now_playing', label: '상영중'},
+//   { to: '/movies/top_rated', label: '평점 높은'},
+//   { to: '/movies/upcoming', label: '개봉 예정'},
+// ]
 
 export const Navbar = () => {
   // LINKS.map(...) : 배열을 순회하면서 NavLink생성
---
+++ ellie/week3/mission3/src/constants/LINKS.ts
@@ -0,0 +1,11 @@
+// 메뉴 데이터 배열 : 네비게이션 항목을 객체 배열로
+//  - to : 이동할 경로(URL)
+//  - label : 화면에 표시될 텍스트
+export const LINKS = [
+  { to: '/', label: '홈'},
+  { to: '/movies/popular', label: '인기 영화'},
+  { to: '/movies/now_playing', label: '상영중'},
+  { to: '/movies/top_rated', label: '평점 높은'},
+  { to: '/movies/upcoming', label: '개봉 예정'},
+]
+// 상수로 분리 
