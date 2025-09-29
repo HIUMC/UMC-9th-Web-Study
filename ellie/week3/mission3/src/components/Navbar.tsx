b9e0bfb404d45bb5f35216587f5d68170f896877
jangmk05 <jangmk2005@naver.com>
2025-09-29 10:10:40 +0900

    Navbar 기존 상수 배열 제거

--- ellie/week3/mission3/src/components/Navbar.tsx
+++ ellie/week3/mission3/src/components/Navbar.tsx
@@ -1,17 +1,6 @@
 import {NavLink } from "react-router-dom"
 import { LINKS } from "../constants/LINKS"
 
-// 메뉴 데이터 배열 : 네비게이션 항목을 객체 배열로
-//  - to : 이동할 경로(URL)
-//  - label : 화면에 표시될 텍스트
-// const LINKS = [
-//   { to: '/', label: '홈'},
-//   { to: '/movies/popular', label: '인기 영화'},
-//   { to: '/movies/now_playing', label: '상영중'},
-//   { to: '/movies/top_rated', label: '평점 높은'},
-//   { to: '/movies/upcoming', label: '개봉 예정'},
-// ]
-
 export const Navbar = () => {
   // LINKS.map(...) : 배열을 순회하면서 NavLink생성
   //  - NavLink? : 내비게이션 메뉴에서 활성화된 링크를 표시할 때 사용
