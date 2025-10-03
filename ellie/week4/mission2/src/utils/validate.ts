ebba6a38616e07ac7460ec3ea096e2821e3acdb5
jangmk05 <jangmk2005@naver.com>
2025-10-03 13:43:24 +0900

    mission2 완료

---
+++ ellie/week4/mission2/src/utils/validate.ts
@@ -0,0 +1,28 @@
+export type userSigninInformation = {
+  email: string;
+  password: string;
+}
+
+function validateUser(values:userSigninInformation){
+  const errors = {
+    email:"",
+    password:"",
+  };
+
+  if(!/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i.test(values.email,)){
+    errors.email="올바른 이메일 형식이 아닙니다."
+  }
+
+  if(!(values.password.length >= 8 && values.password.length < 20)) {
+    errors.password="비밀번호는 8~20자 사이로 입력해주세요.";
+  }
+
+  return errors;
+}
+
+// 로그인 유효성 검사
+function validateSignin(values: userSigninInformation) {
+  return validateUser(values);
+}
+
+export default validateSignin;
