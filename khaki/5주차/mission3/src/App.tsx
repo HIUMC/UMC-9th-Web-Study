51f40b008d0e57583df8a1a43c0a307728c6e413
Seony777 <addiolsw1@gmail.com>
2025-10-26 22:58:21 +0900

    feat: mission3 완료

---
+++ khaki/5주차/mission3/src/App.tsx
@@ -0,0 +1,70 @@
+import { createBrowserRouter, RouterProvider, type RouteObject } from "react-router-dom"
+import "./App.css"
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import LoginPage from "./pages/LoginPage";
+import HomeLayout from "./layouts/HomeLayout";
+import SignupPage from "./pages/SignupPage";
+import MyPage from "./pages/MyPage";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedLayout from "./layouts/ProtectedLayout";
+import GoogleLoginRedirectPage from "./pages/GoogleLoginRedirectPage";
+
+// 1. 홈페이지
+// 2. 로그인 페이지
+// 3 회원가입 페이지
+
+// publicRoutes: 인증이 필요없는(로그인 안해도 되는) 페이지들
+const publicRoutes:RouteObject[] = [
+  {
+    path: "/",  
+    element: <HomeLayout/>,   //여기에는 보통 공통된 레이아웃이 들어감
+    errorElement: <NotFoundPage/>,
+    children:[  // nav와 footer는 고정되고 가운데 부분만 경로에 따라 바뀜
+      {
+        index: true,
+        element: <HomePage/> 
+      },
+      {
+        path: 'login',
+        element: <LoginPage/>
+      },
+      {
+        path: 'signup',
+        element: <SignupPage/>
+      },
+      {
+        path: 'v1/auth/google/callback',
+        element:<GoogleLoginRedirectPage/>
+      }
+    ]
+  },
+]
+
+// privateRoutes: 인증이 필요한(로그인 해야만 되는) 페이지들
+const privateRoutes:RouteObject[] = [
+  {
+    path:'/',
+    element:<ProtectedLayout/>,
+    errorElement:<NotFoundPage/>,
+    children:[
+      {
+        path:'my',
+        element:<MyPage/>
+      }
+    ] 
+  }
+  ]
+
+const router = createBrowserRouter([...publicRoutes,...privateRoutes]);
+
+function App() {
+
+  return (
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>  
+  )
+}
+
+export default App
---
+++ khaki/5주차/mission3/src/context/AuthContext.tsx
@@ -0,0 +1,102 @@
+// <AuthContext: 인증 관련 전역상태 관리 컨텍스트>
+import { createContext, useContext, useState, type PropsWithChildren } from "react";
+import type { RequestSigninDto } from "../types/auth";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { LOCAL_STORAGE_KEY } from "../constants/key";
+import { postSignin } from "../apis/auth";
+
+// 1. AuthContext에서 다룰 데이터 타입 정의
+interface AuthContextType {
+  accessToken: string | null;
+  refreshToken: string | null;
+  login:(signInData:RequestSigninDto)=> Promise<void>;
+  logout:() => Promise<void>;
+}
+
+// 2. AuthContext 생성
+export const AuthContext = createContext<AuthContextType>({
+  accessToken: null,
+  refreshToken: null,
+  login: async()=>{},
+  logout: async()=>{},
+});
+
+// 3. Context를 실제로 감싸서 전역상태를 공급하는 Provider 컴포넌트 정의(상태+함수+value 포함)
+export const AuthProvider = ({children}:PropsWithChildren) => {
+  const{
+    getItem:getAccessTokenFromStorage,
+    setItem:setAccessTokenInStorage,
+    removeItem:removeAccessTokenFromStorage,
+  } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
+
+  const{
+    getItem:getRefreshTokenFromStorage,
+    setItem:setRefreshTokenInStorage,
+    removeItem:removeRefreshTokenFromStorage,
+  } = useLocalStorage(LOCAL_STORAGE_KEY.refreshToken);
+
+  const [accessToken, setAccessToken] = useState<string | null>(
+    getAccessTokenFromStorage(),
+  );
+  const [refreshToken, setRefreshToken] = useState<string | null>(
+    getRefreshTokenFromStorage(),
+  );
+
+  const login = async (signInData:RequestSigninDto) => {
+    try
+    {
+      const {data} = await postSignin(signInData);
+
+      if(data){
+        const newAccessToken = data.accessToken;
+        const newRefreshToken = data.refreshToken;
+
+        setAccessTokenInStorage(newAccessToken);
+        setRefreshTokenInStorage(newRefreshToken);
+
+        setAccessToken(newAccessToken);
+        setRefreshToken(newRefreshToken);
+        alert("로그인 성공");
+        window.location.replace("/my"); 
+      }
+    }
+    catch (error) 
+    {
+      console.error("로그인 오류",error);
+      alert("로그인에 실패했습니다. 다시 시도해주세요.");
+    };
+  };
+
+  const logout = async () => {
+    try{
+      removeAccessTokenFromStorage();
+      removeRefreshTokenFromStorage();
+
+      setAccessToken(null);
+      setRefreshToken(null);
+
+      alert("로그아웃 되었습니다.");
+    }
+    catch (error) {
+      console.error("로그아웃 오류",error);
+      alert("로그아웃에 실패했습니다. 다시 시도해주세요.");
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{accessToken, refreshToken, login, logout}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if(!context)
+    throw new Error("useAuth는 AuthProvider 내부에서만 사용 가능합니다.");
+
+  return context;
+}
+
+
+
