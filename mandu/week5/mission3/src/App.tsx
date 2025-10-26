cdc4098f18802063c1722abd48ddbe16d1c29529
tablemin03 <vs201466@naver.com>
2025-10-26 19:31:45 +0900

    week5/mission3 제출

---
+++ mandu/week5/mission3/src/App.tsx
@@ -0,0 +1,70 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  type RouteObject,
+} from "react-router-dom";
+import RootLayout from "./layout/root-layout";
+import SignupPage from "./pages/signupPage";
+import LoginPage from "./pages/loginPage";
+import "./App.css";
+import SignupPw from "./pages/passwordPage";
+import NamePage from "./pages/namePage";
+import MyPage from "./pages/myPage";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedLayout from "./layout/protexted-layout";
+import GoogleLoginRedirectPage from "./pages/GoogleLoginRedirectPage";
+
+const publicRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/signup",
+        element: <SignupPage />,
+      },
+      {
+        path: "/pw",
+        element: <SignupPw />,
+      },
+      {
+        path: "/name",
+        element: <NamePage />,
+      },
+      {
+        path: "/v1/auth/google/callback",
+        element: <GoogleLoginRedirectPage />,
+      },
+    ],
+  },
+];
+
+const protectedRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <ProtectedLayout />,
+    children: [
+      {
+        path: "my",
+        element: <MyPage />,
+      },
+    ],
+  },
+];
+
+function App() {
+  const router = createBrowserRouter([...publicRoutes, ...protectedRoutes]);
+  return (
+    <>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </>
+  );
+}
+
+export default App;
