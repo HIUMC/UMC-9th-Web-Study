407f6e3583cbfebecd4a2daad6d8efc7a9cb68c2
tablemin03 <vs201466@naver.com>
2025-11-02 04:35:16 +0900

    week6/실습1 직접 만들기

---
+++ mandu/week6/mission1/src/App.tsx
@@ -0,0 +1,13 @@
+import { WelcomeData } from "./components/UserDataDisplay";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <WelcomeData />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
