200932093f4cf9a9ad4bc8ff9b075353f7774827
tablemin03 <vs201466@naver.com>
2025-11-06 18:24:49 +0900

    refactor: 토글버튼 컴포넌트 분리

--- mandu/week7/mission1/src/components/Comments/Comment.tsx
+++ mandu/week7/mission1/src/components/Comments/Comment.tsx
@@ -4,6 +4,7 @@ import { PAGINATION_ORDER } from "../../enums/common";
 import { useInView } from "react-intersection-observer";
 import { useParams } from "react-router-dom";
 import CommentSkeletonList from "./CommentSkeletonList";
+import ToggleButton from "../Buttons/ToggleButton";
 
 const Comment = () => {
   const { lpid } = useParams();
@@ -31,38 +32,7 @@ const Comment = () => {
           댓글 ({data?.pages[0].data.data.length > 0 ? "전체" : "0"})
         </h3>
 
-        <div className="flex justify-end items-center mb-4">
-          <div className="inline-flex rounded-md shadow-sm" role="group">
-            <button
-              type="button"
-              className={`
-              px-4 py-2 text-sm font-medium rounded-l-lg transition-colors duration-200 border-white-600
-              ${
-                !asc
-                  ? "bg-white text-black"
-                  : "bg-black text-white border  hover:bg-gray-600"
-              }
-            `}
-              onClick={() => setAsc(false)}
-            >
-              오래된 순
-            </button>
-            <button
-              type="button"
-              className={`
-              px-4 py-2 text-sm font-medium rounded-r-lg transition-colors duration-200 border-white-600
-              ${
-                asc
-                  ? "bg-white text-black"
-                  : "bg-black text-white border  hover:bg-gray-600"
-              }
-            `}
-              onClick={() => setAsc(true)}
-            >
-              최신 순
-            </button>
-          </div>
-        </div>
+        <ToggleButton asc={asc} setAsc={setAsc} />
       </div>
 
       <div className="space-y-4">
--- mandu/week7/mission1/src/pages/HomePage.tsx
+++ mandu/week7/mission1/src/pages/HomePage.tsx
@@ -6,6 +6,7 @@ import { useInView } from "react-intersection-observer";
 import { Link } from "react-router-dom";
 import LpCard from "../components/LpCard/LpCard";
 import LpCardSkeletonList from "../components/LpCard/LpCardSkeletonList";
+import ToggleButton from "../components/Buttons/ToggleButton";
 
 const HomePage = () => {
   const [search, setSearch] = useState("");
@@ -52,38 +53,7 @@ const HomePage = () => {
   return (
     <div className="container mx-auto px-4 py-6">
       {/* <input value={search} onChange={(e) => setSearch(e.target.value)} /> */}
-      <div className="flex justify-end items-center mb-4">
-        <div className="inline-flex rounded-md shadow-sm" role="group">
-          <button
-            type="button"
-            className={`
-              px-4 py-2 text-sm font-medium rounded-l-lg transition-colors duration-200 border-white-600
-              ${
-                !asc
-                  ? "bg-white text-black"
-                  : "bg-black text-white border  hover:bg-gray-600"
-              }
-            `}
-            onClick={() => setAsc(false)}
-          >
-            오래된 순
-          </button>
-          <button
-            type="button"
-            className={`
-              px-4 py-2 text-sm font-medium rounded-r-lg transition-colors duration-200 border-white-600
-              ${
-                asc
-                  ? "bg-white text-black"
-                  : "bg-black text-white border  hover:bg-gray-600"
-              }
-            `}
-            onClick={() => setAsc(true)}
-          >
-            최신 순
-          </button>
-        </div>
-      </div>
+      <ToggleButton asc={asc} setAsc={setAsc} />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {lps?.pages
