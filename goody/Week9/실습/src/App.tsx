90700441bbc070fff3063ea42332d2faad2bff1d
goody <ehdgusqaz98@gmail.com>
2025-11-19 20:22:11 +0900

    useReducer 실습 완료

---
+++ goody/Week9/실습/src/App.tsx
@@ -0,0 +1,13 @@
+import UseReducerCompany from "./pages/UseReducerCompany"
+import UseReducerPage from "./pages/useReducerPage"
+
+function App() {
+
+  return (
+    <>
+      <UseReducerCompany />
+    </>
+  )
+}
+
+export default App
