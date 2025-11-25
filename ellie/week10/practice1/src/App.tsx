d0e9ba3d7ab58c5e925e64fb143b7b8aab5f6630
jangmk05 <jangmk05@gmail.com>
2025-11-25 09:50:15 +0900

    practice1 완료

---
+++ ellie/week10/practice1/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import UseCallback from "./pages/useCallback";
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center h-dvh">
+      <UseCallback />
+    </div>
+  );
+}
+
+export default App;
