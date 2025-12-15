4ad5f3ae217c8758c5075346f56a7ce02f9ebb3e
Cuyqo <saon0410@gmail.com>
2025-12-16 05:51:01 +0900

    fix: lp사이트 최적화

--- roppi/week8/mission3/src/hooks/queries/useGetInfiniteLpList.ts
+++ roppi/week8/mission3/src/hooks/queries/useGetInfiniteLpList.ts
@@ -14,7 +14,7 @@ return useInfiniteQuery({
   queryFn: ({ pageParam }) =>
     getLpList({ cursor: pageParam, limit, search, order }),
   initialPageParam: 0,
-  getNextPageParam: (lastPage) => lastPage.data.nextCursor, 
+  getNextPageParam: (lastPage) => lastPage.data.nextCursor,
   staleTime: 0,
   gcTime: 1000 * 60 * 3,
 });
