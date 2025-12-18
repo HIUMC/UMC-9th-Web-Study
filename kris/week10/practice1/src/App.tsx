3bf8e9b17b18f99f3fb29a5458240f4fab9f3277
dorhy <dorhyory@gmail.com>
2025-12-18 15:07:55 +0900

    week10 practice1 완료

---
+++ kris/week10/practice1/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import UseCallbackPage from "./pages/UseCallbackPage";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <UseCallbackPage />
+      </div>
+    </>
+  );
+}
+
+export default App;
